6ffbb699c107389049e8ee160fc59de942a4ecd9
Andrea Hernandez <andreajhl29@gmail.com>
2023-12-14 06:15:27 -0300

    CLIENT: add Select component UI

---
+++ client/src/components/ui/select/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Form } from 'react-bootstrap';
+
+const Select = ({
+  handleSelect,
+  optionsSelect,
+  defaultValue,
+}) => {
+  const handleChange = ({ target: { value }}) => handleSelect(value);
+
+  return (
+    <Form className='w-md-25'>
+      <Form.Select
+        defaultValue={defaultValue}
+        onChange={handleChange}
+      >
+        {
+          optionsSelect.map(item => (
+            <option key={item} value={item}>{item}</option>
+          ))
+        }
+        <option value={defaultValue}>{defaultValue}</option>
+      </Form.Select>
+    </Form>
+  );
+};
+
+export default Select;
