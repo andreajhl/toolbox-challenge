f82f387f74a57f3bcd2f4cdc5beabc64809798ac
Andrea Hernandez <andreajhl29@gmail.com>
2023-12-14 06:07:25 -0300

    CLIENT: add wordings

---
+++ client/src/wordings/index.js
@@ -0,0 +1,20 @@
+const wordings = {
+  title: 'React Test App',
+  table: {
+    headers: [
+      'File Name',
+      'Text',
+      'Number',
+      'Hex',
+    ],
+  },
+  alerts: {
+    error: {
+      title: 'Upss! Ocurrio un error',
+      content: 'No conseguimos informacion sobre este file'
+    }
+  },
+  loading: 'Loading',
+};
+
+export default wordings;
